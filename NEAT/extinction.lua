e7bef0488db2e170dcfd7d11db3168ccb88aede1
Nikolai Savas <nikosavas@hotmail.com>
2015-11-07 11:02:38 -0500

    Added algorithm to handle species extinction

---
+++ NEAT/extinction.lua
@@ -0,0 +1,20 @@
+--A species goes extinct if it's been the worst species for a while
+function extinction(allSpecies)
+	local keepSpeciesCount = math.floor(#allSpecies*EXTINCTION_CANDIDATE)
+
+	table.sort(allSpecies, function(a,b)
+		return a.fitness > b.fitness
+	end)
+
+	--For each of the species that aren't making the bar
+	for s=keepSpeciesCount,#allSpecies do
+		local species = allSpecies[s]
+
+		species.extinctionCounter += 1
+
+		if species.extinctionCounter >= EXTINCTION_GENERATIONS then
+			--Gotta kill it
+			table.remove(allSpecies,s)
+		end
+	end
+end
