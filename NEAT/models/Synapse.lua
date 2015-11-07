c13774657ad0d0ef28cd9323efe3ef218f4efa06
Nikolai Savas <nikosavas@hotmail.com>
2015-11-07 01:30:04 -0500

    Added Synapse model

---
+++ NEAT/models/Synapse.lua
@@ -0,0 +1,22 @@
+Synapse = {}
+
+currentSynapse = 0
+
+function Synapse:new()
+	currentSynapse++
+
+	local synapse = {}
+
+	-- ID of the synapse
+	synapse.label = currentSynapse
+
+	-- Set to IDs of Neurons
+	synapse.from = 0
+	synapse.to = 0
+
+	-- Weight of the connection
+	synapse.weight = 0.0
+
+	-- Whether the synapse is being expressed or not
+	synapse.enabled = true
+end
