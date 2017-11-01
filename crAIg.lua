3fd7a3264f74bb8bbe2738dd89d019bf8d930a85
Niko Savas <niko@savas.ca>
2017-11-01 09:55:13 -0400

    crAIg will not be auto-loaded from a previous generation by default.

--- crAIg.lua
+++ crAIg.lua
@@ -36,7 +36,7 @@ table.insert(seedSpecies.genomes,randomGenome)
 table.insert(crAIg.species,seedSpecies)
 
 --Uncomment to load a previous iteration of crAIg
-crAIg = persistence.load("runtime/crAIgs/crAIg-saved-39.lua")
+--crAIg = persistence.load("runtime/crAIgs/crAIg-saved-39.lua")
 
 local numGenerations = 1000
 for i=1,numGenerations do
